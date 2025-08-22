a170aeccc0a52ad0abfa178e9f33e648bbcd8be4
ahk0413 <rladkgusgd@gmail.com>
2025-08-22 20:00:58 +0900

    [design] header ui 작업

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -1,11 +1,28 @@
 import { Outlet } from 'react-router';
+import Header from './shared/components/Header';
+import Footer from './shared/components/Footer';
+import { useState } from 'react';
 
+const TABS = [
+  {id: 'all', label:'전체'},
+  {id: 'play', label:'투표/게임'},
+]
 // 프로젝트 레이아웃
 const RootLayout = () => {
+
+  const [tab, setTab] = useState('all')
+
   return (
-    <>
-      <Outlet />
-    </>
+    <div>
+      <Header tabs={TABS} currentTab={tab} onTabChange={(tabId:string) => setTab(tabId)} />
+      <div className=''>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+
+      <Footer />
+    </div>
   );
 };
 export default RootLayout;
