7624d4d557d8f4544e5474447f31042ecdc1bc15
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-22 15:10:17 +0900

    [file] 파비콘 및 로고 추가

--- src/shared/components/Input/index.tsx
+++ src/shared/components/Input/index.tsx
@@ -1,10 +1,12 @@
 import tw from '@/shared/utils/style';
-import TimeSvg from './assets/time.svg';
+import clockSVG from '@/assets/icon/clock-15.svg';
+import deleteSVG from '@/assets/icon/delete-15.svg';
 
 interface Props {
   className?: string;
   showLabel?: boolean;
   showTime?: boolean;
+  showDelete?: boolean;
   label: string;
 }
 
@@ -12,6 +14,7 @@ const Input = ({
   className,
   showLabel = true,
   showTime = false,
+  showDelete = false,
   label,
 }: Props) => {
   return (
@@ -19,9 +22,13 @@ const Input = ({
       <label className={tw(showLabel ? `text-sm ${className}}` : 'sr-only')}>
         {label}
       </label>
-      <div className="max-w-3xl w-full p-3 border-1 border-gray-light rounded-sm h-12">
-        {!showTime && <img src={TimeSvg} />}
+      <div
+        className="flex
+       max-w-3xl w-full p-3 border-1 border-gray-light rounded-sm h-12"
+      >
+        {!showTime && <img src={clockSVG} />}
         <input />
+        {!showDelete && <img src={deleteSVG} />}
       </div>
     </div>
   );
