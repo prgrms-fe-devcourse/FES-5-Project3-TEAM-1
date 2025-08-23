22488377e5e78669798ade366e05c8c669b7a35f
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-23 17:26:58 +0900

    [feat] 공통 인풋, 텍스트 에어리어, 체크박스 구현 및 쇼케이스 페이지 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,15 @@ interface ImportMetaEnv {
   VITE_SUPABASE_URL: string;
   VITE_SUPABASE_ANON_KEY: string;
 }
+
+declare module '*.svg' {
+  import React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+declare module '*.svg?react' {
+  import React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
