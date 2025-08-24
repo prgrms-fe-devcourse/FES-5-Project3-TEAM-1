fcf734a88623213a9cbe2e0fbd73afd96483d4d2
ahk0413 <rladkgusgd@gmail.com>
2025-08-24 18:05:39 +0900

    [feature] feedsInput  radio, focus관련, 글자수제한 기능 추가

--- src/shared/components/FeedsInput/index.tsx
+++ src/shared/components/FeedsInput/index.tsx
@@ -4,7 +4,7 @@ import VoteSVG from '@/assets/icon/vote-16.svg?react';
 import BalanceSVG from '@/assets/icon/balance-16.svg?react';
 import DrawSVG from '@/assets/icon/draw-16.svg?react';
 import Button from '../Button';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const feedOptions = [
   { id: 'pic', icon: PicSVG, label: '사진', type: 'file' },
@@ -20,86 +20,120 @@ const feedOptionsContent: Record<string, React.ReactNode> = {
 };
 
 function FeedsInput() {
+  const [isFocused, setIsFocused] = useState(false);
   const [selectedChkbox, setSelectedChkbox] = useState<string | null>(null);
+  const [textareaText, setTextareaText] = useState('');
+  const feedsInputRef = useRef<HTMLDivElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const textLength = 200;
+
+  const handleCountText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextareaText(e.target.value.slice(0, textLength));
+  };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        feedsInputRef.current &&
+        !feedsInputRef.current.contains(event.target as Node)
+      ) {
+        setIsFocused(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
 
   return (
-    <div className="flex flex-col p-5 rounded-xl shadow-[0_4px_8px_0_rgba(0,0,0,0.20)]">
-      <div className="flex flex-col relative w-full">
+    <div
+      ref={feedsInputRef}
+      className="flex flex-col p-5 rounded-xl shadow-[0_4px_8px_0_rgba(0,0,0,0.20)]"
+    >
+      <div className="flex flex-col relative w-full z-">
         <TextareaAutoSize
           min-rows={1}
           name=""
           id=""
-          maxLength={200}
+          ref={textareaRef}
+          value={textareaText}
+          maxLength={textLength}
+          onChange={handleCountText}
+          onFocus={() => setIsFocused(true)}
           placeholder="익명으로 자유롭게 의견을 나눠보세요."
-          className="pr-7 py-3 w-full min-h-12 resize-none overflow-y-scroll [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden focus:outline-none focus-visible:ring-1 focus-visible:ring-primary-light"
+          className="pr-7 py-3 w-full min-h-12 resize-none overflow-y-scroll [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden focus:outline-none"
         >
           {/* 여기에다가 사진 선택시 미리보기 하면 됨 */}
         </TextareaAutoSize>
-        <span className="block absolute right-0 bottom-0 ml-auto text-gray-dark">
-          200
-        </span>
+        {isFocused && (
+          <span className="block absolute right-0 bottom-0 ml-auto text-gray-dark">
+            {textLength - textareaText.length}
+          </span>
+        )}
       </div>
-      <div className="flex justify-between items-center mt-5">
-        <ul className="flex gap-2">
-          {feedOptions.map(({ id, icon: Icon, label, type }) => (
-            <li key={id}>
-              {type === 'file' ? (
-                <>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    id={`feedOptions-${id}`}
-                    className="hidden"
-                  />
-                  <label
-                    htmlFor={`feedOptions-${id}`}
-                    className="flex-center gap-1 px-2 py-0.5 bg-secondary rounded-2xl cursor-pointer transition-colors duration-150 ease-in-out hover:bg-primary-light"
-                  >
-                    <Icon aria-hidden />
-                    <span className="text-sm">{label}</span>
-                  </label>
-                </>
-              ) : (
-                <>
-                  <input
-                    type="checkbox"
-                    name="feedOptions"
-                    id={`feedOptions-${id}`}
-                    checked={selectedChkbox === id}
-                    onChange={() =>
-                      setSelectedChkbox(selectedChkbox === id ? null : id)
-                    }
-                    aria-controls="feedsContent"
-                    className="hidden"
-                  />
-                  <label
-                    htmlFor={`feedOptions-${id}`}
-                    className={`flex-center gap-1 px-2 py-0.5 rounded-2xl cursor-pointer transition-colors duration-150 ease-in-out hover:bg-primary-light ${selectedChkbox === id ? 'bg-primary' : 'bg-secondary'}
-                    `}
-                  >
-                    <Icon aria-hidden />
-                    <span className="text-sm">{label}</span>
-                  </label>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-
-        <Button size="sm" color="blue">
-          올리기
-        </Button>
-      </div>
-
       <div
-        id="feedsContent"
-        role="region"
-        aria-live="polite"
-        aria-atomic="true"
-        aria-label="선택된 옵션"
-        className="mt-3 pt-5 border-t-1 border-dashed border-gray-dark"
+        className={`transition-all duration-300 ease-in-out ${isFocused || selectedChkbox ? 'overflow-visible max-h-96' : 'overflow-hidden max-h-0'}`}
       >
-        {selectedChkbox && feedOptionsContent[selectedChkbox]}
+        <div className="flex justify-between items-center pt-5">
+          <ul className="flex gap-2">
+            {feedOptions.map(({ id, icon: Icon, label, type }) => (
+              <li key={id}>
+                {type === 'file' ? (
+                  <label
+                    htmlFor={`feedOptions-${id}`}
+                    className="flex-center gap-1 px-2 py-0.5 bg-secondary rounded-2xl cursor-pointer transition-colors duration-150 ease-in-out active:bg-primary  hover:bg-primary-light focus-within:outline-1 focus-within:outline-slate-900"
+                  >
+                    {' '}
+                    <input
+                      type="file"
+                      accept="image/*"
+                      id={`feedOptions-${id}`}
+                      className="sr-only"
+                    />
+                    <Icon aria-hidden />
+                    <span className="text-sm">{label}</span>
+                  </label>
+                ) : (
+                  <label
+                    htmlFor={`feedOptions-${id}`}
+                    className={`flex-center gap-1 px-2 py-0.5 rounded-2xl cursor-pointer transition-colors duration-150 ease-in-out hover:bg-primary-light focus-within:outline-1 focus-within:outline-slate-900 ${selectedChkbox === id ? 'bg-primary' : 'bg-secondary'}
+                      `}
+                  >
+                    <input
+                      type="checkbox"
+                      name="feedOptions"
+                      id={`feedOptions-${id}`}
+                      checked={selectedChkbox === id}
+                      onChange={() =>
+                        setSelectedChkbox(selectedChkbox === id ? null : id)
+                      }
+                      aria-controls="feedsContent"
+                      className="sr-only"
+                    />
+                    <Icon aria-hidden />
+                    <span className="text-sm">{label}</span>
+                  </label>
+                )}
+              </li>
+            ))}
+          </ul>
+
+          <Button size="sm" color="blue">
+            올리기
+          </Button>
+        </div>
+
+        {selectedChkbox && (
+          <div
+            id="feedsContent"
+            role="region"
+            aria-live="polite"
+            aria-atomic="true"
+            aria-label="선택된 옵션"
+            className="mt-3 pt-5 border-t-1 border-dashed border-gray-dark "
+            style={{ maxHeight: selectedChkbox ? '1000px' : '0px' }}
+          >
+            {feedOptionsContent[selectedChkbox]}
+          </div>
+        )}
       </div>
     </div>
   );
