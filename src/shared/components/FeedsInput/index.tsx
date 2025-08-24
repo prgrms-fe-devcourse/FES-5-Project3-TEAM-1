23a0c6b5dc387bfcb9bac81e9f744640ea16c886
ahk0413 <rladkgusgd@gmail.com>
2025-08-24 12:10:53 +0900

    [test] prettier 확인

---
+++ src/shared/components/FeedsInput/index.tsx
@@ -0,0 +1,30 @@
+import { useRef } from "react"
+
+function FeedsInput() {
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
+
+  const handleTextarea = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    const target = e.target;
+    target.style.height = "48px"
+    target.style.height = target.scrollHeight + "px"
+  }
+
+
+  return (
+    <div className="flex p-5 rounded-xl shadow-[0_4px_8px_0_rgba(0,0,0,0.20)]">
+      <div className="flex flex-col relative w-full">
+        <textarea ref={textareaRef} onInput={handleTextarea} rows={1} name="" id="" maxLength={200} placeholder="익명으로 자유롭게 의견을 나눠보세요." className="pr-7 py-3 w-full min-h-12 resize-none overflow-y-scroll [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden"></textarea>
+        <span className="block absolute right-0 bottom-0 ml-auto">200</span>
+
+
+  
+  
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+export default FeedsInput
