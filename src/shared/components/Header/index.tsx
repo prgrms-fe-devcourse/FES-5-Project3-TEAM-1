9e6166f8185b2ffde68eeaba2022dbdc45a74d0e
ahk0413 <rladkgusgd@gmail.com>
2025-08-25 11:32:30 +0900

    [design] header height 축소로 인한 내부 전체 수정, 전체적인 레이아웃 height 길이 수정

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import { Link, NavLink } from 'react-router';
 import logo from '@/assets/logo.png';
 import moonSVG from '@/assets/icon/moon-20.svg';
-import settingsSVG from '@/assets/icon/settings-36.svg';
-import usersSVG from '@/assets/icon/users-20.svg';
+import settingsSVG from '@/assets/icon/settings-32.svg';
+import usersSVG from '@/assets/icon/users-16.svg';
 import clsx from 'clsx';
 import { useState } from 'react';
 
@@ -15,13 +15,15 @@ interface Props {
 function Header({ tabs, currentTab, onTabChange }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="flex items-center md:flex-nowrap relative w-full px-5 pt-2 md:h-20 h-auto flex-wrap justify-between gap-2 md:gap-4 shadow-slate-200 shadow-md z-50">
-      <Link to="/" className="order-1">
+    <header className="flex fixed left-0 top-0 justify-between items-center flex-wrap md:flex-nowrap gap-2 md:gap-4 w-full h-auto md:h-15 px-5 pt-2 md:pt-0 bg-white shadow-slate-200 shadow-md z-50">
+      <Link to="/" className="order-1 w-25">
         <img src={logo} alt="Anonimo" />
       </Link>
+
+      {/* tab 메뉴 */}
       <nav
         role="tablist"
-        className="flex order-3 md:order-2 mx-auto gap-6 w-full h-18 md:w-auto "
+        className="flex order-3 md:order-2 mx-auto gap-6 w-full md:w-auto h-10 md:h-full "
       >
         {tabs.map((tab) => (
           <button
@@ -29,7 +31,7 @@ function Header({ tabs, currentTab, onTabChange }: Props) {
             aria-selected={currentTab === tab.id}
             key={tab.id}
             className={clsx(
-              'text-xl flex-1 md:flex-auto',
+              'text-md md:text-xl flex-1 md:flex-auto',
               currentTab === tab.id
                 ? 'font-bold border-b-2 border-b-slate-900'
                 : '',
@@ -41,14 +43,15 @@ function Header({ tabs, currentTab, onTabChange }: Props) {
         ))}
       </nav>
 
-      <div className="flex gap-4 order-2 md:order-3">
+      {/* 추가 메뉴 */}
+      <div className="flex gap-2 md:gap-4 order-2 md:order-3">
         <button
-          className="flex justify-center items-center w-9 h-9 rounded-full bg-secondary"
+          className="flex justify-center items-center w-8 h-8 rounded-full bg-secondary"
           aria-label="다크 모드"
         >
           <img src={moonSVG} aria-hidden="true" className="w-5 h-5" />
         </button>
-        <div className="flex justify-center items-center gap-1 min-w-20 bg-primary rounded-4xl">
+        <div className="flex justify-center items-center gap-1 min-w-16 bg-primary rounded-4xl">
           <img
             className="flex w-5 h-5"
             src={usersSVG}
@@ -73,11 +76,12 @@ function Header({ tabs, currentTab, onTabChange }: Props) {
         </button>
       </div>
 
+      {/* 설정 dropdown 메뉴 */}
       <div
         tabIndex={-1}
         aria-label="설정 메뉴"
         className={clsx(
-          'absolute top-full -mt-15 md:mt-2 right-5 flex flex-col justify-between px-3 py-2 w-50 h-60 rounded-xl bg-white shadow-2xl transition-all duration-400 ease-out',
+          'absolute top-full -mt-8 md:mt-2 right-4 flex flex-col justify-between px-3 py-2 w-50 h-60 rounded-xl bg-white shadow-[0_4px_8px_0_rgba(0,0,0,0.20)] transition-all duration-400 ease-out',
           isOpen
             ? 'opacity-100 translate-y-0'
             : 'opacity-0 -translate-y-3 pointer-events-none',
