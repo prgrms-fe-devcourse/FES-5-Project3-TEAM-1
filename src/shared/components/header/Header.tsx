817a1e83f248eaca7b07b7fbf4e090fd8f1b2774
heroson02 <heroson0925@gmail.com>
2025-08-26 17:33:21 +0900

    feat : 헤더 로그인/로그아웃 조건부 렌더링 설정

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -8,6 +8,8 @@ import { useRef, useState } from 'react';
 import { useCloseOnOutsideOrEsc } from '@/shared/hook/useCloseOnOutsideOrEsc';
 import Button from '../button/Button';
 import { useModal } from '@/shared/utils/ModalProvider';
+import { useAuth } from '@/shared/utils/AuthProvider';
+import useLogout from '@/features/login/hooks/useLogout';
 
 interface Props {
   tabs?: { id: string; label: string }[];
@@ -20,7 +22,7 @@ function Header({ tabs, currentTab, onTabChange, onOpenCreateModal }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const settingsMenuRef = useRef<HTMLDivElement>(null);
   const modal = useModal();
-
+  const loginAuth = useAuth();
   /* 훅 이용 (esc or 밖 클릭 시 settingsMenu 닫힘) */
   useCloseOnOutsideOrEsc<HTMLDivElement>({
     ref: settingsMenuRef,
@@ -125,14 +127,26 @@ function Header({ tabs, currentTab, onTabChange, onOpenCreateModal }: Props) {
               >
                 방 생성하기
               </Button>
-              <Button
-                size="sm"
-                color="default"
-                fullWidth
-                onClick={() => modal.openModal('login')}
-              >
-                로그인
-              </Button>
+              {loginAuth.userId && (
+                <Button
+                  size="sm"
+                  color="default"
+                  fullWidth
+                  onClick={useLogout()}
+                >
+                  로그아웃
+                </Button>
+              )}
+              {!loginAuth.userId && (
+                <Button
+                  size="sm"
+                  color="default"
+                  fullWidth
+                  onClick={() => modal.openModal('login')}
+                >
+                  로그인
+                </Button>
+              )}
             </div>
           </div>
         </div>
