74079ccbb39820951f17df912b803226580356a8
ahk0413 <rladkgusgd@gmail.com>
2025-08-26 21:13:57 +0900

    [feat] 지우기, 배경색, 새그림판 기능 구현

--- src/features/drawing/ui/DrawingForm.tsx
+++ src/features/drawing/ui/DrawingForm.tsx
@@ -1,13 +1,13 @@
 import clsx from 'clsx';
 import type { KonvaEventObject } from 'konva/lib/Node';
 import { useEffect, useRef, useState } from 'react';
-import { Stage, Layer, Line } from 'react-konva';
+import { Stage, Layer, Rect, Line } from 'react-konva';
 import BackSVG from '@/assets/icon/back-24.svg?react';
 import EraserSVG from '@/assets/icon/eraser-24.svg?react';
 import FillSVG from '@/assets/icon/fill-24.svg?react';
 import RefreshSVG from '@/assets/icon/refresh-24.svg?react';
 
-type Tool = 'pen' | 'eraser';
+type Tool = 'pen' | 'eraser' | 'fill';
 
 const colors = [
   { name: '빨강', value: '#EF4444' },
@@ -31,12 +31,13 @@ function DrawingForm() {
   const [tool, setTool] = useState<Tool>('pen');
   const [lines, setLines] = useState<Lines[]>([]);
   const [selectedColor, setSelectedColor] = useState(colors[0].value);
+  const [fill, setFill] = useState('#ffffff');
   const isDrawing = useRef(false);
 
   const drawContainerRef = useRef<HTMLDivElement>(null);
   const [stageWidth, setStageWidth] = useState(0);
 
-  /* stage에게 width값 100%를 직접 줄수가 없어 부모 길이를 찾아줘서 넣어야함ㅠ px단위로 들어가야한다고함 휴 */
+  /* Stage는 width값 px로 밖에 안들어가 반응형을 위해 부모 width값 따라서 width값 받을 수 있도록... */
   useEffect(() => {
     const updateWidth = () => {
       if (drawContainerRef.current?.offsetWidth) {
@@ -51,6 +52,11 @@ function DrawingForm() {
   }, []);
 
   const handleMouseDown = (e: KonvaEventObject<MouseEvent | TouchEvent>) => {
+    if (tool === 'fill') {
+      handleFill(selectedColor);
+      return;
+    }
+
     isDrawing.current = true;
     const pos = e.target.getStage()?.getPointerPosition();
     if (!pos) return;
@@ -81,8 +87,38 @@ function DrawingForm() {
     isDrawing.current = false;
   };
 
+  const handleChangeColor = (color: string) => {
+    setSelectedColor(color);
+    setTool('pen');
+  };
+
+  const handleFill = (color: string) => {
+    setFill(color);
+    setTool('fill');
+  };
+
+  const handleReset = () => {
+    setLines([]);
+    setFill('#ffffff');
+    setTool('pen');
+  };
+
+  const ChangeCursor = () => {
+    switch (tool) {
+      case 'pen':
+        return `url(/src/assets/icon/pen-24.svg) 0 0, auto`;
+      case 'fill':
+        return `url(/src/assets/icon/fill-24.svg) 0 0, auto`;
+      case 'eraser':
+        return `url(/src/assets/icon/eraser-24.svg) 0 0, auto`;
+      default:
+        return 'default';
+    }
+  };
+
   return (
     <div ref={drawContainerRef} className="relative w-full">
+      <p className="text-xl mb-5">그림 그리기</p>
       <div className="flex">
         <Stage
           onMouseDown={handleMouseDown}
@@ -92,17 +128,19 @@ function DrawingForm() {
           onTouchMove={handleMouseMove}
           onTouchEnd={handleMouseUp}
           width={stageWidth}
-          height={300}
-          className="order-2"
+          height={320}
+          style={{ cursor: ChangeCursor() }}
+          className="h-fit p-0 order-2 border-1 border-gray rounded-sm"
         >
           <Layer>
+            <Rect x={0} y={0} width={stageWidth} height={320} fill={fill} />
             {lines.map((line, i) => (
               <Line
                 key={i}
                 points={line.points}
                 stroke={line.color}
                 strokeWidth={5}
-                tension={0.5}
+                tension={0.1}
                 lineCap="round"
                 lineJoin="round"
                 globalCompositeOperation={
@@ -118,11 +156,16 @@ function DrawingForm() {
             <li key={i}>
               <button
                 type="button"
-                onClick={() => console.log('color 클릭')}
+                onClick={() => {
+                  handleChangeColor(value);
+                }}
                 aria-label={name}
                 className={clsx(
                   'w-6 h-6 rounded-full',
                   name === '흰색' ? 'border-1 border-black' : '',
+                  selectedColor === value &&
+                    tool === 'pen' &&
+                    'ring-2 ring-black',
                 )}
                 style={{ backgroundColor: value }}
               ></button>
@@ -130,13 +173,36 @@ function DrawingForm() {
           ))}
         </ul>
       </div>
-      <ul className="flex justify-end gap-3">
-        <li>
-          <button type="button" aria-label="지우개">
-            <EraserSVG aria-hidden />
-          </button>
-        </li>
-      </ul>
+      <div className="flex justify-end gap-2">
+        <button
+          type="button"
+          title="지우개"
+          aria-label="지우개"
+          onClick={() => {
+            setTool('eraser');
+          }}
+        >
+          <EraserSVG aria-hidden />
+        </button>
+        <button
+          type="button"
+          title="배경색 바꾸기"
+          aria-label="배경 색 바꾸기"
+          onClick={() => {
+            setTool('fill');
+          }}
+        >
+          <FillSVG aria-hidden />
+        </button>
+        <button
+          type="button"
+          title="새 그림판"
+          aria-label="새 그림판"
+          onClick={handleReset}
+        >
+          <RefreshSVG aria-hidden />
+        </button>
+      </div>
     </div>
   );
 }
