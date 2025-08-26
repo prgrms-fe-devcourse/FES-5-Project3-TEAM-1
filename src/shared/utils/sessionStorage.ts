4cfad4d3ad43c397092bee8c40c58121a318ece4
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-26 09:17:07 +0900

    [feat] 세션 유틸 기능 구현

---
+++ src/shared/utils/sessionStorage.ts
@@ -0,0 +1,18 @@
+export const sessionStorageUtil = () => ({
+  getSession: <T>(key: string): T | null => {
+    try {
+      const data = JSON.parse(sessionStorage.getItem(key) || '[]');
+      return data ? JSON.parse(data) : null;
+    } catch (error) {
+      throw new Error(`세션 정보를 불러오는 중 에러 발생 : ${error}`);
+    }
+  },
+
+  setSession: <T>(key: string, value: T): void => {
+    try {
+      sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      throw new Error(`세션 정보를 저장하는 중 에러 발생 : ${error}`);
+    }
+  },
+});
