ba402dee522d8ab29f0f132c533728f1d9e3625c
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-29 17:33:58 +0900

    [fix] comment 실시간 반영 및 그림 피드 fetch

---
+++ src/features/comment/component/CommentInput.tsx
@@ -0,0 +1,59 @@
+import Button from '@/shared/components/button/Button';
+import Input from '@/shared/components/Input';
+import { getNicknameFromSession } from '@/shared/utils/nickname';
+import { useRef } from 'react';
+
+interface Props {
+  addComment: (content: string, nickname: string) => void;
+}
+
+const CommentInput = ({ addComment }: Props) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleAdd = () => {
+    if (!inputRef.current) return;
+
+    const value = inputRef.current.value.trim();
+
+    if (value) {
+      const nickname = getNicknameFromSession() ?? 'nimo';
+      addComment(value, nickname);
+      inputRef.current.value = '';
+    }
+  };
+
+  return (
+    <div className="flex gap-2">
+      <Input
+        label="댓글 입력"
+        aria-label="댓글 입력"
+        placeholder="댓글을 입력해 주세요."
+        showLabel={false}
+        ref={inputRef}
+        className="flex-1 bg-white"
+        onKeyDown={(e) => {
+          // mac OS 일때는 Composing 방지
+          if (e.nativeEvent.isComposing) {
+            return;
+          }
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            handleAdd();
+          }
+        }}
+      />
+      <Button
+        type="button"
+        size="default"
+        color="default"
+        onClick={handleAdd}
+        aria-label="댓글 등록"
+        tabIndex={0}
+        className="min-w-auto md:min-w-[80px]"
+      >
+        등록
+      </Button>
+    </div>
+  );
+};
+export default CommentInput;
