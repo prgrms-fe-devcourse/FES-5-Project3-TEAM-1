6b463e08c8338e026da0081190b5cb73bb3db1a3
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-29 10:06:48 +0900

    [fix] getBrowserTokenFromSession 함수 스펠링 미스 수정

--- src/shared/utils/token.ts
+++ src/shared/utils/token.ts
@@ -3,7 +3,7 @@ import { sessionStorageUtil } from './sessionStorage';
 const ANONIMO_TOKEN = 'anonimo_token';
 
 // 토큰 get
-export const getBrowserTokenFormSession = (): string | null => {
+export const getBrowserTokenFromSession = (): string | null => {
   return sessionStorageUtil.getSession<string>(ANONIMO_TOKEN);
 };
 // 토근 set
