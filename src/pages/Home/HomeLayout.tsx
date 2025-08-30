ae3d421c3a415544ece0177f637384c5b6e5204d
ahk0413 <rladkgusgd@gmail.com>
2025-08-30 13:24:13 +0900

    [feat] 랜딩페이지 틀 제작, router 수정

---
+++ src/pages/Home/HomeLayout.tsx
@@ -0,0 +1,36 @@
+import { useRef, useEffect } from 'react';
+import HeroSection01 from './HeroSection01';
+import HeroSection02 from './HeroSection02';
+import type { HeroSectionProps } from './type/Hero';
+import { Lighter } from './Lighter';
+
+export default function HomeLayout() {
+  const section01Ref = useRef<HeroSectionProps>(null);
+  const section02Ref = useRef<HeroSectionProps>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      const vh = window.innerHeight;
+
+      // Section01
+      if (section01Ref.current) {
+        const progress = Math.min(scrollTop / vh, 1);
+        section01Ref.current.tl?.progress(progress);
+      }
+
+      // Section02(가로스크롤)는 ScrollTrigger가 내부에서 모든 애니메이션 관리
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div>
+      <Lighter />
+      <HeroSection01 ref={section01Ref} />
+      <HeroSection02 ref={section02Ref} />
+    </div>
+  );
+}
