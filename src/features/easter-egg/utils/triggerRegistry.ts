d55800c5c8ca72df1138e1abe7ecb1395be1e4df
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-01 14:46:07 +0900

    [feat] 차를 탄 모모 에그터 에그 설정

--- src/features/easter-egg/utils/triggerRegistry.ts
+++ src/features/easter-egg/utils/triggerRegistry.ts
@@ -1,10 +1,12 @@
 import type { ComponentType } from 'react';
 import Beom from '../components/Beom';
 import ConfettiComponent from '../components/Confetti';
+import MomoWithCar from '../components/MomoWithCar';
 
 export const COMPONENT_REGISTRY: Record<string, ComponentType<any>> = {
   teacher: Beom,
   congrats: ConfettiComponent,
+  car: MomoWithCar,
 };
 
 export const getComponentByCategory = (category: string) => {
