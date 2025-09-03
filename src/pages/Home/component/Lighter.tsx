62f2654bdc85691a66527b8213cf4285b78bd45a
ahk0413 <rladkgusgd@gmail.com>
2025-09-03 09:29:12 +0900

    [fix] lighter 렌더링 이슈로 수정(useState => useRef)

--- src/pages/Home/component/Lighter.tsx
+++ src/pages/Home/component/Lighter.tsx
@@ -1,29 +1,39 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const Lighter = () => {
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const lighterRef = useRef<HTMLDivElement>(null);
+  const frameRef = useRef<number | null>(null);
 
-  // 마우스 위치 추적
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      setMousePos({ x: e.clientX, y: e.clientY });
-    };
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => window.removeEventListener('mousemove', handleMouseMove);
-  }, []);
+      if (!lighterRef.current) return;
 
-  const style = {
-    background: `radial-gradient(600px at ${mousePos.x}px ${mousePos.y}px, rgba(6, 182, 212, 0.12), transparent 80%)`,
-    // background: `radial-gradient(600px at ${mousePos.x}px ${mousePos.y}px, rgba(6, 182, 212, 0.12), transparent 80%)`,
-    // background: `radial-gradient(600px at ${mousePos.x}px ${mousePos.y}px, rgba(251, 146, 60, 0.1), transparent 80%)`,
-  };
+      if (frameRef.current != null) {
+        cancelAnimationFrame(frameRef.current);
+      }
+
+      frameRef.current = requestAnimationFrame(() => {
+        lighterRef.current!.style.background = `
+          radial-gradient(600px at ${e.clientX}px ${e.clientY}px, rgba(6,182,212,0.12), transparent 80%)
+        `;
+      });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      if (frameRef.current !== null) {
+        cancelAnimationFrame(frameRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div
-      className="pointer-events-none fixed inset-0 z-[9999] transition duration-300"
-      style={style}
+      ref={lighterRef}
+      className="pointer-events-none fixed inset-0 z-[9999]"
     />
   );
 };
